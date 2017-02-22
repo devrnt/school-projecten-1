e1faf1584cda2d4a49fe46db4b3b6cca0deaf011
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2017-02-22 12:55:50 +0100

    package "persistentie" gamaakt. Klasse "Connectie" gemaakt en String gemaakt met connectie.

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,11 @@
+package persistentie;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Connectie {
+
+    public static final String JDBC_URL = "jdbc:mysql://ID222177_g14.db.webhosting.be?user=ID222177_g14&password=ucIm2ojDollX";
+
+}
