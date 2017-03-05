011e0bf3819e5bec8847feda780031932f2f18de
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2017-03-05 21:51:08 +0100

    KartRepository aangepast

--- src/domein/KaartRepository.java
+++ src/domein/KaartRepository.java
@@ -5,10 +5,26 @@
  */
 package domein;
 
+import persistentie.KaartMapper;
+
 /**
  *
  * @author Juan Carlos
  */
 public class KaartRepository {
+    private final KaartMapper mapper;
+
+    public KaartRepository() {
+        mapper = new KaartMapper();
+        //spelers = new ArrayList<>();
+    }
+    
+    public void voegToe(Kaart kaart, Speler speler){
+        //kijken of er al default kaarten zijn toegevoegd
+        //s
+        //
+        
+        mapper.voegToe(kaart, speler);
+    }
     
 }
