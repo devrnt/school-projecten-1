e587dc5806f853ad225622060bbaf5c90ae3e56a
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2017-03-05 21:40:07 +0100

    SpelerRepository afgewerkt.

--- src/domein/SpelerRepository.java
+++ src/domein/SpelerRepository.java
@@ -1,9 +1,28 @@
-
 package domein;
 
+import persistentie.SpelerMapper;
+
 public class SpelerRepository {
-    public void voegSpelerToe(Speler speler){
-        
+
+    private final SpelerMapper mapper;
+
+    public SpelerRepository() {
+        mapper = new SpelerMapper();
+        //spelers = new ArrayList<>();
     }
+
+    public void voegSpelerToe(Speler speler) {
+        if (bestaatSpeler(speler.getGebruikersnaam())) {
+            throw new IllegalArgumentException("gebruikersnaam al in gebruik");//exception nog maken GebruikersnaamInGebruikException
+        }
+
+        mapper.voegToe(speler);
+
+    }
+
+    private boolean bestaatSpeler(String gebruikersnaam) {
+        return mapper.geefSpeler(gebruikersnaam) != null;
+    }
+    
 
 }
