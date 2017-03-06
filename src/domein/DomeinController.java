bdd32734afb39cbebe096c12f3045d645e7b7343
642328ed <642328ed@gmail.com>
2017-03-06 11:25:28 +0100

    Domeincontroller aangevuld
    methode maakSpeler aangemaakt (naam hiervan kan verkeerd zijn, vergelijk
    met DCD)

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,12 +12,24 @@ import java.util.Calendar;
  * @author Juan Carlos
  */
 public class DomeinController {
-    
-    
-    
-    
-    
-     //later doen met reguliere expresies.
+
+    private SpelerRepository spelerRepository;
+    private KaartRepository kaartRepository;
+
+    public DomeinController() {
+        spelerRepository = new SpelerRepository();  //het aanmaken van de repositorys gebeurt hier zodat ze niet elke keer opnieuw aangemaakt moeten worden
+        kaartRepository = new KaartRepository();
+    }
+
+    public void maakSpeler(String naam, int geboortejaar) {
+        controleerGebruikersnaam(naam);
+        controleerLeeftijd(geboortejaar);
+        Speler nieuweSpeler = new Speler(naam, geboortejaar, 0);
+        //stapel kaarten moet nog toegevoegd worden
+        spelerRepository.voegSpelerToe(nieuweSpeler); //nog surrounden met try-catch
+    }
+
+    //later doen met reguliere expresies.
     public void controleerGebruikersnaam(String gebruikersnaam) {
         if (gebruikersnaam.length() < 3) {
             throw new IllegalArgumentException("Je voornaam moet minstens uit 3 karakters bestaan.");
@@ -34,9 +46,8 @@ public class DomeinController {
         if (Character.isDigit(gebruikersnaam.charAt(0))) {
             throw new IllegalArgumentException("Uw voornaam mag niet beginnen met een cijfer");
         }
-        
-    }
 
+    }
 
     public void controleerLeeftijd(int geboortejaar) {
         int leeftijd = Calendar.getInstance().get(Calendar.YEAR) - geboortejaar;
@@ -47,5 +58,4 @@ public class DomeinController {
         }
     }
 
-    
 }
