b69d2f2108c0de44d6cd7bc86acffad9031d1576
642328ed <642328ed@gmail.com>
2017-03-07 10:50:24 +0100

    Voegtoe kaartmapper afgewerkt
    het toevoegen van kaarten in de database afgewerkt, het opvragen moet
    nog gebeuren

--- src/persistentie/KaartMapper.java
+++ src/persistentie/KaartMapper.java
@@ -10,6 +10,7 @@ import domein.Speler;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -27,7 +28,16 @@ public class KaartMapper {
             query.setString(3, kaart.getType().split("/")[0] + kaart.getType().split("/")[1]);
             query.executeUpdate();
             
-
+            PreparedStatement spelerlijst = conn.prepareStatement("SELECT * FROM ID222177_dobbel.speler WHERE gebruikersnaam = ?");
+            spelerlijst.setString(1, speler.getGebruikersnaam());
+            ResultSet rs = spelerlijst.executeQuery();
+            int spelerId = rs.getInt("spelerID");
+            
+            PreparedStatement query2 = conn.prepareStatement("INSERT INTO ID222177_g14.kaart (spelerid, omschrijving)"
+                    + "VALUES (?, ?)");
+            query2.setInt(1, spelerId);
+            query2.setString(2, kaart.getOmschrijving());
+            query2.executeUpdate();
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
