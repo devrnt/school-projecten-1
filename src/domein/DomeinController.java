51302ae55647ee78cc706eac3d791c3c0e59997e
642328ed <642328ed@gmail.com>
2017-03-08 17:36:29 +0100

    Verplaatsen validatie
    Validatie van controller verplaatst naar speler, en de exception
    messages aangepast naar die in de language files

--- src/domein/DomeinController.java
+++ src/domein/DomeinController.java
@@ -22,50 +22,19 @@ public class DomeinController {
         kaartRepository = new KaartRepository();
     }
 
-    public void maakSpeler(String naam, int geboortejaar) {
-        try {
-            controleerGebruikersnaam(naam);
-            controleerLeeftijd(geboortejaar);
-        }catch(IllegalArgumentException e){ //kan ook custom exceptions gebruiken, of exception die erven van IlligalArgumentException
-            System.out.println(e.getMessage());
-            return; //stopt de methode, de speler wordt niet aangemaakt
-        }
+    public void maakSpeler(String naam, int geboortejaar) {        
         Speler nieuweSpeler = new Speler(naam, geboortejaar, 0);
-        //stapel kaarten moet nog toegevoegd worden
+        
+        //validatie gebruikersnaam
         try{
+            nieuweSpeler.controleerGebruikersnaam(naam);
+            nieuweSpeler.controleerLeeftijd(geboortejaar);
+        }catch(IllegalArgumentException ex){
+            ex.printStackTrace();
+            return;
+        }
+        //stapel kaarten moet nog toegevoegd worden
         spelerRepository.voegSpelerToe(nieuweSpeler); //nog surrounden met try-catch
-        }catch(GebruikernaamInGebruikException gige){
-            System.out.println(gige.getMessage());  //veranderen door een boodschap op de ui
-        }
+        
     }
-
-    //later doen met reguliere expresies.
-    public void controleerGebruikersnaam(String gebruikersnaam) {
-        if (gebruikersnaam.length() < 3) {
-            throw new IllegalArgumentException("Je voornaam moet minstens uit 3 karakters bestaan.");
-        }
-        if (gebruikersnaam.contains(" ")) {
-            throw new IllegalArgumentException("Uw voornaam mag geen spaties bevatten.");
-        }
-
-        //Controle maken voor leestekes in naam
-        //if(voornaam.matches("\\p{Punct}")){
-        //    throw new IllegalArgumentException("Uw voornaam mag geen leestekens bevatten.");
-        //}
-        //alleen nodig bij voornaam
-        if (Character.isDigit(gebruikersnaam.charAt(0))) {
-            throw new IllegalArgumentException("Uw voornaam mag niet beginnen met een cijfer");
-        }
-
-    }
-
-    public void controleerLeeftijd(int geboortejaar) {
-        int leeftijd = Calendar.getInstance().get(Calendar.YEAR) - geboortejaar;
-        if (leeftijd < 6) {
-            throw new IllegalArgumentException("U moet minstens 6 jaar zijn.");
-        } else if (leeftijd > 99) {
-            throw new IllegalArgumentException("U moet jonger zijn dan 99 jaar.");
-        }
-    }
-
 }
--- src/domein/Speler.java
+++ src/domein/Speler.java
@@ -38,4 +38,31 @@ public class Speler {
         return geboortejaar;
     }
 
+    //later doen met reguliere expresies.
+    public void controleerGebruikersnaam(String gebruikersnaam) {
+        if (gebruikersnaam.length() < 3) {
+            throw new IllegalArgumentException("naam_te_kort");
+        }
+        if (gebruikersnaam.contains(" ")) {
+            throw new IllegalArgumentException("verkeerd_character");
+        }
+
+        //Controle maken voor leestekes in naam
+        if(gebruikersnaam.matches("\\p{Punct}")){
+            throw new IllegalArgumentException("verkeerd_character");
+        }
+        
+        if (Character.isDigit(gebruikersnaam.charAt(0))) {
+            throw new IllegalArgumentException("start_cijfer");
+        }
+    }
+    
+    public void controleerLeeftijd(int geboortejaar) {
+        int leeftijd = Calendar.getInstance().get(Calendar.YEAR) - geboortejaar;
+        if (leeftijd < 6) {
+            throw new IllegalArgumentException("te_jong");
+        } else if (leeftijd > 99) {
+            throw new IllegalArgumentException("te_oud");
+        }
+    }
 }
