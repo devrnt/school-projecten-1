55daaae1276dc73b318ebedece4c13ac9875bc69
637330jv <Jonas.vandegehuchte.y7330@student.hogent.be>
2017-05-02 16:29:26 +0200

    UC5 + vertaling

---
+++ src/startup/UC5.java
@@ -0,0 +1,30 @@
+package startup;
+
+import domein.DomeinController;
+
+/**
+ *
+ * @author Jonas
+ */
+public class UC5 {
+    
+        public static void testUC5(DomeinController dc) {
+              
+        dc.maakWedstrijd();
+        int aantalGewonnenSetsSpeler1 = 0;
+        int aantalGewonnenSetsSpeler2 = 0;
+        
+//         if(aantalGewonnenSetsSpeler1 < 3 && aantalGewonnenSetsSpeler2 < 3){
+//             UC6.testUC6(dc);
+//             if(?){
+//                 aantalGewonnenSetsSpeler1++;
+//             }else
+//                 aantalGewonnenSetsSpeler2++;                     
+//         }
+         
+        dc.verhoogKrediet();
+        
+    }    
+}
+    
+
