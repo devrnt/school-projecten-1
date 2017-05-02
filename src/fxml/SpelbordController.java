d58136e15e6f038a696e8222648fe4b15ce1ab8e
642328ed <642328ed@gmail.com>
2017-05-02 16:06:32 +0200

    spelbord ui

--- src/fxml/SpelbordController.java
+++ src/fxml/SpelbordController.java
@@ -8,13 +8,32 @@ package fxml;
 import domein.DomeinController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 /**
  *
  * @author Edward
  */
 public class SpelbordController implements Initializable{
+    
+    @FXML
+    public Label setCounter;
+    public Label score1;
+    public Label score2;
+    public GridPane spelbord1;
+    public GridPane spelbord2;
+    public GridPane hand;
+    public Button saveButton;
+    public Button wisselButton;
+    public Button endTurnButton;
+    public Button passButton;
+    
     
     private DomeinController dc;
     
@@ -25,7 +44,36 @@ public class SpelbordController implements Initializable{
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        
+        
+        saveButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        wisselButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+        endTurnButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+            }
+        });
+        
+        passButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
     }
     
 }
